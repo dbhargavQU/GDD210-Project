d62cab75cecbff839d46e5eeb7e1381ec7724696
artoth <116179747+artoth@users.noreply.github.com>
2023-04-10 18:49:14 -0400

    Sounds2.0
    Redo Files with all the folders in them, room one background music, keypad noise. Audio manager script. Also empty game object called sounds for audio manager.

---
+++ Assets/Audio/AudioManager/AudioManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    private AudioSource audioSource;
+
+    private void OnEnable()
+    {
+        instance = this;
+    }
+
+    private void OnDisable()
+    {
+        instance = null;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(AudioClip clip, float volume = 1f)
+    {
+        instance.audioSource.PlayOneShot(clip, volume);
+    }
+
+    public static void PlaySoundAtPosition(AudioClip clip, Vector3 pos, float volume = 1f)
+    {
+        AudioSource.PlayClipAtPoint(clip, pos, volume);
+    }
+}
+
