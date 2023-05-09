85fceedd620d369515e716423c10f603618b65f9
Mohith Bhargav Donthu <dbhargav@quinnipiac.edu>
2023-05-09 16:51:12 -0400

    updated

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -36,4 +36,51 @@ public class PauseMenu : MonoBehaviour
         SceneManager.LoadScene(SceneName);
     }
     
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseScreen;
+
+    public string SceneName;
+    public void Start()
+    {
+        PauseScreen.SetActive(false);
+    }
+    void Update()
+    {
+        // Pauses the game with Esc key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                PauseScreen.SetActive(true);
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        PauseScreen.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneName);
+    }
 }
